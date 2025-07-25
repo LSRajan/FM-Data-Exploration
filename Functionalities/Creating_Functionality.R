8f3b86938d53555580e637b8d384dca7c32ee8ac
t80143lr <lucias.rajan@student.manchester.ac.uk>
2025-07-25 13:20:34 +0100

    Created functions to aid player search

---
+++ Functionalities/Creating_Functionality.R
@@ -0,0 +1,83 @@
+#What if I want a list of players who are for example:
+# At least 180cm, at least 80th percentile for progressive passes, play Center Back and will cost at between £30M - £40M?
+
+#I will do it manually first to see the workflow
+
+min_height <- 180
+min_percentile <- 80
+max_cost <- 40000000
+min_cost <- 30000000
+metric <- "Pr.Passes"
+pos <- "Centre_Back"
+
+#the problem with this is that players can have the same name,
+left_join(performance, player_info, by = "Name")
+#although we should be able to remove this problem by combining name, age, and club which are in both
+#an improvement would be to preserve the ingame UIDs
+performance |> 
+  select(Name, Age, Club) |> 
+  head(1)
+player_info |> 
+  select(Name, Age, Club) |> 
+  head(1)
+
+players <- left_join(performance, player_info, by = c("Name" = "Name", "Club" = "Club", "Age" = "Age"))
+
+#if this works, we should have kept all players from performance
+
+players |> 
+  filter(is.na(Name)) |> 
+  sum()
+players |> 
+  nrow()
+performance |> 
+  nrow()
+
+
+#A look at calculating percentiles
+players |> 
+  mutate(percentile = ntile(get(metric),100)) |>
+  select(Name, all_of(metric), percentile) |>
+  arrange(desc(percentile), desc(get(metric)))
+
+#A manual way to find this player list based on requirements
+players1 <- players |> 
+  mutate(percentile = ntile(get(metric),100))
+
+players1 |> 
+  filter(Height >= min_height & 
+           Lower >= min_cost & 
+           Upper <= max_cost &
+           percentile >= 80 &
+           get(pos) == TRUE
+         ) |> 
+  select(Name, Height, Lower, Upper, Pr.Passes, percentile, all_of(pos))
+
+
+#Complete function that allows to filter through and ifnd targets for a position,
+#Limitations: currently only one position and performance metric,
+#             limited number of restrictions / filters
+
+player_search <- function(min_height, min_age, max_age, min_cost, max_cost, metric, min_percentile, position){
+  players <- left_join(performance, player_info, by = c("Name" = "Name", "Club" = "Club", "Age" = "Age"))
+  
+  players <- players |> 
+    filter(get(position))
+   # mutate(percentile = ntile(get(metric),100))   <---- causes error, unsure why but did it below instead
+    
+  players <- mutate(players, percentile = ntile(players[metric], 100))
+  
+  players |> 
+    filter(Height >= min_height & 
+             Age >= min_age &
+             Age <= max_age &
+             Lower >= min_cost & 
+             Upper <= max_cost &
+             percentile >= min_percentile
+    ) |> 
+    select(Name, Height, Lower, Upper, Age, all_of(metric), percentile, all_of(position)) |> 
+    arrange(desc(percentile), desc(metric))
+}
+
+#player_search(0, 20, 30, 10000000, 60000000, "Gls", 70, "Striker")
+#player_search(0, 20, 30, 0, 1000000000, "ShT", 90, "Striker")
